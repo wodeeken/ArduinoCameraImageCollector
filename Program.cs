e568077dea064596d32002bc555cfc363b013118
will <wdeeken7@yahoo.com>
2023-07-15 18:06:03 -0500

    Code cleanup, added comments. Updated readme.

--- Program.cs
+++ Program.cs
@@ -18,37 +18,36 @@ namespace ArduinoCameraImageCollector
         // Main entry point. 
         static void Main(string[] args)
         {
-            
+            int parsedBaudRate;
             if(args.Length >= 1){
                 if(args[0].Trim() == "--help" ){
-                    Console.Out.WriteLine("Arduino Camera Listener");
-                    Console.Out.WriteLine("An app to read in JPEG image data from the provided serial port, and to write them into the same directory as " +
-                        "this executable. Images will be stored in a folder with the format MM_dd_YY, and image names will be of the form HH_mm_ss");
-                    Console.Out.WriteLine("This application is intended to be used with the Arduino Camera, running the sketch located @ ");
-                    Console.Out.WriteLine("no args: begin listening session on /dev/ttyACM0");
-                    Console.Out.WriteLine("arg 1 : the port attached to the camera.");
-                    Console.Out.WriteLine("arg 2: ");
-                    Console.Out.WriteLine("Note: the port, whether default or user-specified, needs to be opened for reading prior to starting this program, e.g. run 'sudo chmod a+r <port-name>");
+                    Console.Out.WriteLine("ArduinoCameraImageCollector");
+                    Console.Out.WriteLine("An app to read in JPEG image data from the provided serial port and baud rate, and to write them into the same directory as " +
+                        "this executable. Images will be stored in a folder with the format MM_dd_YY, and image names will be of the form HH_mm_ss. ");
+                    Console.Out.WriteLine("Notes:");
+                    Console.Out.WriteLine("1. The port, whether default or user-specified, needs to be opened for reading prior to starting this program, e.g. run 'sudo chmod a+r <port-name>");
+                    Console.Out.WriteLine("2. This application is intended to be used with the Arduino Camera, running the sketch located @ . The baud rate for the Xbee radio is set at 38400.");
+                    Console.Out.WriteLine("");
+                    Console.Out.WriteLine("Usage: ");
+                    Console.Out.WriteLine(" ArduinoCameraImageCollector <Serial_Port> <Baud_Rate>   : run program with provided serial port.");
+                    Console.Out.WriteLine(" ArduinoCameraImageCollector                             : run program with serial port /dev/ttyACM0 and a baud rate of 921600");
+                    
+                    
                     return;
-                }else{
-                    // the first arg should be port.
-                    // open port.
-                    //ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "/bin/bash", Arguments = "sudo chmod a+rw " + args[0].Trim(), }; 
-                    //Process proc = new Process() { StartInfo = startInfo, };
-                    //proc.Start();
-                    Console.Out.WriteLine("Opening port " + args[0].Trim() + " ...");
-                    comPort = new SerialPort(args[0].Trim(), 38400);
+                }else if(args.Length == 2 && Int32.TryParse(args[1], out parsedBaudRate)){
+                    
+                    Console.Out.WriteLine("Opening port " + args[0].Trim() + " with baud rate " + args[1].Trim() + " ...");
+                    comPort = new SerialPort(args[0].Trim(), parsedBaudRate);
                     comPort.NewLine = "\r";
                     comPort.ReadTimeout = 5000;
-                   // comPort.StopBits = StopBits.Two;
+                }else{
+                    Console.Out.WriteLine("Arguments are not in the correct format. Please run 'ArduinoCaneraImageCollector --help' for proper usage.");
                 }
             }else{
-                // assume port is /dev/ttyUSB0
-                //ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "/bin/bash", Arguments = "sudo chmod a+rw /dev/ttyACM0" , }; 
-                //Process proc = new Process() { StartInfo = startInfo, };
-                //proc.Start();
-                Console.Out.WriteLine("Opening port /dev/ttyUSB1 ..." );
-                comPort = new SerialPort("/dev/ttyUSB1", 921600);
+                // assume port is /dev/ttyACM0.
+                
+                Console.Out.WriteLine("Opening port /dev/ttyACM0 ..." );
+                comPort = new SerialPort("/dev/ttyACM0", 921600);
                 comPort.NewLine = "\r";
                 comPort.ReadTimeout = 5000;
             }
@@ -79,35 +78,36 @@ namespace ArduinoCameraImageCollector
                 while(!ShouldExit){
                     Thread.Sleep(250);
                 }
-
-                //Set ShouldExit back to false.
+          
                 ShouldExit = false;
                 ListOccupied = false;
                 OutToFile = new List<byte>();
                 CurrentReadBufferIterationCount = 0;
             }
-            
+            comPort.Close();
+            Console.Out.WriteLine("Goodbye!");
   
-            //comPort.Close();
+           
         
 
 
         }
+        // Image data is received in smaller chunks, and is received faster than we have time to process. Every data receive is placed in a queue, and we wait at most 20 seconds for all data for this
+        // specific image is received. Then, each data receive concatenates data to a static field OutToFile, and when the last data receive recognizes the JPEG end char sequence (FF D9),
+        // the byte list OutToFile is written to a file.  
         static public void DataReceived_Handler(object sender, SerialDataReceivedEventArgs e){
            try{
-                    // take place on queue.
+                // take place on queue.
                 int myInt = Queue.Count + 1;
                 Queue.Enqueue(myInt);
-                // Check if list is busy. If so, wait.
-                // while(ListOccupied){
-                //     Thread.Sleep(15);
-                // }
                 int waitCounter = 0;
                 while(Queue.Peek() != myInt){
                     Thread.Sleep(20);
                     waitCounter++;
                     if(waitCounter > 1000){
-                        //Flush queue and quit.
+                        // We've waited too long.
+                        // Abort this current image read, so as to not hold up other pics.
+                        // Flush queue and quit.
                         Console.Out.WriteLine("Quitting the queue.");
                         int result;
                         while(Queue.TryDequeue(out result)){
@@ -127,7 +127,6 @@ namespace ArduinoCameraImageCollector
                     readBuffer = new byte[bufferSize];
                     if(bufferSize > 0){
                         comPort.Read(readBuffer, 0, bufferSize);
-                        //System.Console.WriteLine(readBuffer[bufferSize - 1].ToString() + " " + readBuffer[bufferSize - 2].ToString());
                         bool isThisLastRead = false;
                         // Check the end of buffer: FF D9
                         if(bufferSize > 1){
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ArduinoCameraImageCollector
+# Arduino Camera Image Collector
+
+An app to read in JPEG image data from the provided serial port and baud rate, and to write them into the same directory as this executable. Images will be stored in a folder with the format MM_dd_YY, and image names will be of the form HH_mm_ss.
+
+# Notes
+1. The port, whether default or user-specified, needs to be opened for reading prior to starting this program, e.g. run 'sudo chmod a+r <port-name>
+2. This application is intended to be used with the Arduino Camera, running the sketch located @ . The baud rate for the Xbee radio is set at 38400.
+
+# Usage
+ ArduinoCameraImageCollector <Serial_Port> <Baud_Rate>   : run program with provided serial port.
+ ArduinoCameraImageCollector                             : run program with serial port /dev/ttyACM0 and a baud rate of 921600
